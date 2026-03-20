ee2929d6b221cf51399de13b0ed907c4a79c2a09
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-03-20 23:27:36 +0000

    Add winning conditions

--- js/script.js
+++ js/script.js
@@ -66,7 +66,21 @@ const gameBoard = (function() {
         boardArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8",]
     }
 
-    function checkWinner() {}
+    function checkWinner() {
+        const winningCombos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        return winningCombos;
+    }
+
+  // your checking logic here
 
   return {
     updateBoard,
