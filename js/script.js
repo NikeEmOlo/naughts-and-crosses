62e30d22289ddaaf82691b87e79b9050da86d85c
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-03-23 19:57:22 +0000

    Add Ui controls for player moves

--- js/script.js
+++ js/script.js
@@ -2,59 +2,51 @@ const gameController = (function() {
     let whosTurn = true;
 
     function startGame() {
-
-        for (let i = 0; i < 9; i++) {
-            takeTurn()
-            const winner = gameBoard.checkWinner()
-
-            if (winner) {
-                gameBoard.displayBoardInConsole()
-                console.log(`${winner} wins!`)
-                gameBoard.resetGameBoard()
-                break
-            }
-        }
-        
+        let player = getCurrent().player;
+        display.updateAnnouncementText(`${player}'s turn`)
     }
 
-    function takeTurn() {
-        const currentPlayer = whosTurn ? "Player 1" : "Player 2";
-        const playerChoice = prompt(`${currentPlayer}'s turn. Choose where to put your marker by typing the number:
-        ${gameBoard.printBoard()}`)
-        //Need to update the gameBoard here
-        if (whosTurn) {
-            gameBoard.updateBoard(playerChoice, "x");
-            whosTurn = !whosTurn;
-        } else if (!whosTurn) {
-            gameBoard.updateBoard(playerChoice, "o");
-            whosTurn = !whosTurn;
+    function takeTurn(cell, cellNum) {
+        let player = getCurrent().player;
+        let marker = getCurrent().marker;
+
+        display.addMarker(cell, marker);
+        gameBoard.updateBoard(cellNum, marker);
+        stats.gameStats.turns++
+        whosTurn = !whosTurn;
+        const winner = gameBoard.checkWinner();
+        if (winner === null) {
+            display.updateAnnouncementText(`${player}'s turn`);
         } else {
-            console.log("Hmmm, try choosing somewhere on the board");
-            playerChoice;
+            display.updateAnnouncementText(`${winner} wins!`)
         }
+
+        if (stats.gameStats.turns === 9) {
+            display.updateAnnouncementText(`It's a draw!`)
+        }
+    }
+
+    function getCurrent() {
+        const current = whosTurn 
+            ? { player: "Player 1", marker: "X" }
+            : { player: "Player 2", marker: "O" }
+        
+        return current
     }
 
     return {
         startGame,
+        whosTurn,
+        takeTurn,
     }
 })()
 
 const gameBoard = (function() {
     let boardArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8",]
 
-    function updateBoard(index, player) {
-        boardArr[index] = player;
+    function updateBoard(index, marker) {
+        boardArr[index] = marker;
         return boardArr;
-    }
-
-    function displayBoardInConsole() {
-        console.log(boardArr.slice(0, 3).join(' | '))
-        console.log(boardArr.slice(3, 6).join(' | '))
-        console.log(boardArr.slice(6, 9).join(' | '))
-    }
-
-    function printBoard() {
-          return `\n${boardArr.slice(0, 3).join(' | ')}\n${boardArr.slice(3, 6).join(' | ')}\n${boardArr.slice(6, 9).join(' | ')}`
     }
 
     function resetGameBoard() {
@@ -92,28 +84,23 @@ const gameBoard = (function() {
 
   return {
     updateBoard,
-    displayBoardInConsole,
-    printBoard,
     resetGameBoard,
     checkWinner,
   }
 })()
 
-const players = (function() {
-    const playerInfo = {
-        p1: {
-            player: "x",
-            score: 0,
-        },
+const stats = (function() {
 
-        p2: {
-            player: "o",
-            score: 0,
+    const gameStats = {
+        score: {
+            p1: 0,
+            p2: 0,
         },
+        turns: 0,
     }
 
     return {
-        playerInfo,
+        gameStats,
     }
 })()
 
@@ -129,7 +116,6 @@ const display = (function() {
         instructionsButton.addEventListener("click", () => {
             modal.showModal();
         } )
-
         closeModalButton.addEventListener("click", () => {
             modal.close();
         })
@@ -137,10 +123,18 @@ const display = (function() {
         //Start the game
         const startGameButton = document.querySelector(".start-game")
         startGameButton.addEventListener("click", () => {
-            gameController.startGame();
             startGameButton.hidden = true;
             displayAnnouncements();
+            gameController.startGame();
         })
+
+        //Listen for player choice
+        const cells = document.querySelectorAll(".cell");
+        for (const cell of cells) {
+            cell.addEventListener("click", (e) => {
+                gameController.takeTurn(e.target, e.target.dataset.index)
+            });
+        }
     }
 
     //Creates a div for announcements to be displayed
@@ -152,7 +146,14 @@ const display = (function() {
     }
 
     function updateAnnouncementText (string) {
+        let announcement = document.querySelector(".announcement")
         announcement.textContent = string;
+    }
+
+    function addMarker(cell, marker) {
+        const text = document.createElement("h1");
+        text.textContent = marker
+        cell.appendChild(text)
     }
 
     
@@ -160,8 +161,9 @@ const display = (function() {
     return {
         eventListeners,
         updateAnnouncementText,
+        addMarker,
+        displayAnnouncements,
     }
 })()
 
 display.eventListeners();
-display.startGame();
